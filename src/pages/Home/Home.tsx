cac24f1b574b25bd5f07b3fc302afa3446687b8c
Rhayann <rhayannbianco@gmail.com>
2023-10-29 13:00:00 -0300

    creating modal menu

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,28 +1,63 @@
-import React from "react";
-import { Header } from "./styles";
-//@ts-ignore
-import { ReactComponent as MenuIcon } from "../../assets/menuIcon.svg";
+import React, { useState } from "react";
+import { Header, ModalContent, ContainerHome, Icon } from "./styles";
+import MenuIcon from "../../assets/menuIcon.svg";
+import { Box, Modal } from "@mui/material";
+import { isMobile } from "react-device-detect";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const Home: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div>
-      <Header>
-        <a>Instagram</a>
-        <a>Github</a>
-        <a>Linkedin</a>
-        <a>
-          Lets talk
-          <img
-            src="https://www.okler.net/previews/porto/9.9.3/img/demos/personal-portfolio-1/svg/arrow.svg"
-            width={25}
-            height={25}
-            alt="Arrow"
-          />
-        </a>
-        <button>
-          <MenuIcon />
+      <Header isMobile={isMobile}>
+        {!isMobile && (
+          <>
+            <a>Instagram</a>
+            <a>Github</a>
+            <a>Linkedin</a>
+            <a>
+              Lets talk
+              <img
+                src="https://www.okler.net/previews/porto/9.9.3/img/demos/personal-portfolio-1/svg/arrow.svg"
+                width={25}
+                height={25}
+                alt="Arrow"
+              />
+            </a>
+          </>
+        )}
+
+        <button onClick={() => setShowMenu(true)}>
+          <img src={MenuIcon} width={25} height={25} alt="Menu" />
         </button>
+        <Modal open={showMenu} onClose={() => setShowMenu(false)}>
+          <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              border: "none",
+            }}
+          >
+            <ModalContent>
+              <Icon onClick={() => setShowMenu(false)}>
+                <CloseIcon />
+              </Icon>
+
+              <a>Home</a>
+              <a>About me</a>
+              <a>Projects</a>
+              <a>Instagram</a>
+              <a>Github</a>
+              <a>Linkedin</a>
+            </ModalContent>
+          </Box>
+        </Modal>
       </Header>
+      <ContainerHome></ContainerHome>
     </div>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,13 +1,18 @@
 import styled from "styled-components";
 
-export const Header = styled.div`
+type HeaderProps = {
+  isMobile?: boolean;
+};
+
+export const Header = styled.div<HeaderProps>`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: ${({ isMobile }) => (isMobile ? "flex-end" : "center")};
   width: 100%;
   height: 58px;
 
   a {
+    font-family: Poppins, sans-serif;
     display: flex;
     align-items: center;
     margin: 0 16px;
@@ -40,7 +45,7 @@ export const Header = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    margin-left: 32px;
+    margin-left: ${({ isMobile }) => (isMobile ? 0 : "32px")};
 
     svg {
       cursor: pointer;
@@ -51,4 +56,34 @@ export const Header = styled.div`
   @media (min-width: 767px) {
     height: 92px;
   }
+`;
+
+export const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-height: 100%;
+  width: fit-content;
+
+  a {
+    font-size: 28px;
+    font-weight: 600;
+    cursor: pointer;
+  }
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  color: white;
+  top: 40px;
+  right: 32px;
+  cursor: pointer;
+`;
+
+export const ContainerHome = styled.div`
+  width: 100%;
+  height: 100%;
 `;
